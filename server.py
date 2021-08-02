432d4e811ef0a3763877d931e3d66e6477b96849
Dmitrii Poshin <prototype6777@icloud.com>
2021-08-02 19:45:04 +0300

    version 1.001

--- server.py
+++ server.py
@@ -9,7 +9,7 @@ from fastapi.responses import Response
 
 app = FastAPI()
 
-SECRET_KEY ="70e8a08fd89d113765859b1f25aeb819e63325e6a625023350b37456a4499b22"
+SECRET_KEY = "70e8a08fd89d113765859b1f25aeb819e63325e6a625023350b37456a4499b22"
 PASSWORD_SALT = "a7eb6439db5bebf9bd9abf496c8a8635b74d36ab1d4123334ef1911f12a914c2"
 
 
@@ -29,11 +29,11 @@ def get_username_from_signed_string(username_signed: str) -> Optional[str]:
     if hmac.compare_digest(valid_sign, sign):
         return username
 
+
 def verify_password(username: str, password: str) -> bool:
     password_hash = hashlib.sha256((password + PASSWORD_SALT).encode()).hexdigest().lower()
     stored_password_hash = users[username]['password'].lower()
-    return password_hash == stored_password_hash 
-
+    return password_hash == stored_password_hash
 
 
 users = {
@@ -49,8 +49,15 @@ users = {
     }
 }
 
-@app.get("/")
-def index_page(username: Optional[str] = Cookie(default=None)):
+
+@app.get('/')
+def index():
+    with open('templates/index.html', 'r') as f:
+        login_page = f.read()
+    return Response(login_page, media_type="text/html")
+
+@app.get("/index2")
+def index_page2(username: Optional[str] = Cookie(default=None)):
     with open('templates/login.html', 'r') as f:
         login_page = f.read()
     if not username:
@@ -62,7 +69,7 @@ def index_page(username: Optional[str] = Cookie(default=None)):
         return response
     try:
         user = users[valid_username]
-    except:
+    except Exception:
         response = Response(login_page, media_type="text/html")
         response.delete_cookie(key="username")
         return response
@@ -73,7 +80,7 @@ def index_page(username: Optional[str] = Cookie(default=None)):
     
 
 @app.post("/login")
-def process_login_page(username : str = Form(...), password : str = Form(...)):
+def process_login_page(username: str = Form(...), password : str = Form(...)):
     user = users.get(username)
     if not user or not verify_password(username, password):
         return Response(
@@ -81,13 +88,25 @@ def process_login_page(username : str = Form(...), password : str = Form(...)):
                 "success": False,
                 "message": "Я Вас не знаю!"
             }),
-             media_type="application/json")
+            media_type="application/json")
     response = Response(
         json.dumps({
             "success": True,
             "message": f"Привет, {user['name']}!<br /> Балансе: {user['balance']}"
         }),
         media_type='application/json')
+
     username_signed = base64.b64encode(username.encode()).decode() + "." + sign_data(username)
     response.set_cookie(key="username", value=username_signed)
     return response
+
+
+@app.post("/logout")
+def process_logout():
+
+    with open('templates/loginpage.html', 'r') as f:
+        login_page = f.read()
+    response = Response(login_page, media_type="text/html")
+    response.delete_cookie(key="username")
+
+
